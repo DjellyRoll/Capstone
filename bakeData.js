a3d0b0f1e9dcb31c6c13783b9b512c51290fed0b
DjellyRoll <patrick.mccall@gmail.com>
2015-04-04 15:52:34 -0700

    Initial Commit

---
+++ bakeData.js
@@ -0,0 +1,50 @@
+﻿var treeData = [
+    {
+        "name": "Do I have the right ingredients?",
+        "parent": "null",
+        "children":
+            [
+                {        
+                    "name": "Purchase Ingredients.",
+                    "parent": "Do I have the right ingredients?"
+                },
+                {        
+                    "name": "Do I have the right tools?",
+                    "parent": "Do I have the right ingredients?",
+                    "children":[
+                        {
+                            "name": "Get an oven",
+                            "parent": "Do I have the right tools?"
+                        },
+                        {
+                            "name": "Purchase baking tools.",
+                            "parent": "Do I have the right tools?"
+                        },
+                        {
+                            "name": "Grease cake pan",
+                            "parent": "Do I have the right tools?",
+                            "children":[
+                                        {
+                                            "name": "Preheat oven",
+                                            "parent": "Grease cake pan",
+                                            "children":[
+                                                        {
+                                                            "name": "Mix ingredients",
+                                                            "parent": "Preheat oven",
+                                                            "children":[
+                                                                        {
+                                                                            "name": "Bake Cake",
+                                                                            "parent": "Mix ingredients"
+                                                                        }
+                                                            ]
+                                                        }
+                                            ]
+                                        }
+                                     ]
+                        }
+                    ]
+                }
+            ]
+    }
+]
+;
